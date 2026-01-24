47942105ce08aa104b2204fbced1e156a8b29978
AdityaSingh251 <singhadityaa1503@gmail.com>
2026-01-24 13:37:45 +0530

    cors url

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 8000;
 
 app.use(cors({
-    origin: 'https://user-dashboardmyappfrontend.vercel.app/',
+    origin: 'https://user-dashboardmyappfrontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true
 }));
