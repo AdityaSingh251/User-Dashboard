f01f3efe8b9e421477ff60a7da071ca7070d90cb
AdityaSingh251 <singhadityaa1503@gmail.com>
2026-01-24 14:05:22 +0530

    'url

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -6,3 +6,26 @@ if(token){
 const emailInput = document.getElementById('email-input');
 const passwordInput = document.getElementById('password-input');
 const form = document.querySelector('form');
+
+form.addEventListener('submit', async function(e){
+    e.preventDefault();
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    const response = await fetch('https://user-dashboard-myappbackend.vercel.app/login', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify({email, password})
+    });
+    const data = await response.json();
+    if(data.status === 'false'){
+        return alert(`❌ ${data.err} ❌`);
+    }
+    alert(data.message);
+    localStorage.setItem('token', data.token);
+    window.location.href = 'https://user-dashboardmyappfrontend.vercel.app/manageUsers.html';
+    emailInput.value = "";
+    passwordInput.value = "";
+});
